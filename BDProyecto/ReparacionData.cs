c58ff688c1b90e7cf43ae8f4533bb89917457930
JorMath <jorman.chuquer@epn.edu.ec>
2024-03-02 19:13:41 -0500

    Fixed Reparacion

--- BDProyecto/ReparacionData.cs
+++ BDProyecto/ReparacionData.cs
@@ -12,19 +12,20 @@ namespace BDProyecto
 {
     public class ReparacionData
     {
-        public static int insertar_cliente(Reparacion reparacion_Quito, Conexion conexion)
+        public static int insertar_reparacion(Reparacion reparacion, string conexion)
         {
+            SqlConnection sqlConnection = new SqlConnection(conexion); 
+            sqlConnection.Open();
             int retorno = 0;
-            using (conexion.obtener_Conexion())
+            using (sqlConnection)
             {
-                conexion.abrir_Conexion();
-                string query = "insert into reparacion_Quito (placa, cod_reparacion, tipo_reparacion, precio, fecha_reparacion, observaciones)" +
-                    $"values ({reparacion_Quito.placa},{reparacion_Quito.cod_reparacion}, {reparacion_Quito.cod_taller}, {reparacion_Quito.tipo_reparacion},{reparacion_Quito.precio},{reparacion_Quito.fecha_reparacion},{reparacion_Quito.observaciones})";
-                SqlCommand cmd = new SqlCommand(query, conexion.obtener_Conexion());
+                string query = "insert into reparacion (placa, cod_reparacion, cod_taller, tipo_reparacion, precio, fecha_reparacion, observaciones)" +
+                    $"values ({reparacion.placa},{reparacion.cod_reparacion}, {reparacion.cod_taller}, {reparacion.tipo_reparacion},{reparacion.precio},{reparacion.fecha_reparacion},{reparacion.observaciones})";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
                 retorno = cmd.ExecuteNonQuery();
 
             }
-            conexion.cerrar_Conexion();
+            sqlConnection.Close();
             return retorno;
         }
         public static List<Reparacion> mostrar_reparacion(string conexion)
@@ -54,27 +55,28 @@ namespace BDProyecto
             }
 
         }
-        public static int actualizar_datos_reparacion_Quito(Reparacion reparacion_Quito, Conexion conexion)
+        public static int actualizar_datos_reparacion_Quito(Reparacion reparacion, string conexion)
         {
+            SqlConnection sqlConnection = new SqlConnection (conexion);
+            sqlConnection.Open();
             int retorno = 0;
-            using (conexion.obtener_Conexion())
+            using (sqlConnection)
             {
-                conexion.abrir_Conexion();
-                string query = $"update reparacion_Quito set placa = {reparacion_Quito.placa},cod_reparacion={reparacion_Quito.cod_reparacion},cod_taller={reparacion_Quito.cod_taller},tipo_reparacion={reparacion_Quito.tipo_reparacion},precio={reparacion_Quito.precio},fecha_reparacion={reparacion_Quito.fecha_reparacion},observaciones={reparacion_Quito.observaciones} from reparacion_Quito where" +
-                    $"placa = {reparacion_Quito.placa} and cod_reparacion={reparacion_Quito.cod_reparacion}";
-                SqlCommand cmd = new SqlCommand(query, conexion.obtener_Conexion());
+                string query = $"update reparacion_Quito set tipo_reparacion={reparacion.tipo_reparacion},precio={reparacion.precio},fecha_reparacion={reparacion.fecha_reparacion},observaciones={reparacion.observaciones} from reparacion where" +
+                    $"placa = {reparacion.placa} and cod_reparacion={reparacion.cod_reparacion} and cod_taller = {reparacion.cod_taller}";
+                SqlCommand cmd = new SqlCommand(query, sqlConnection);
                 retorno = cmd.ExecuteNonQuery();
             }
-            conexion.cerrar_Conexion();
+            sqlConnection.Close();
             return retorno;
         }
-        public static int eliminar_reparacion_Quito(Reparacion reparacion_Quito, Conexion conexion)
+        public static int eliminar_reparacion_Quito(Reparacion reparacion, Conexion conexion)
         {
             int retorno = 0;
             using (conexion.obtener_Conexion())
             {
                 conexion.abrir_Conexion();
-                string query = $"delete from reparacion_Quito where nplaca = {reparacion_Quito.placa} and cod_reparacion={reparacion_Quito.cod_reparacion}";
+                string query = $"delete from reparacion_Quito where nplaca = {reparacion.placa} and cod_reparacion={reparacion.cod_reparacion}";
                 SqlCommand cmd = new SqlCommand(query, conexion.obtener_Conexion());
                 retorno = cmd.ExecuteNonQuery();
             }
