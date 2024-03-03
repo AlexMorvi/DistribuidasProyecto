c4a4605ced3209b027aa686504a5d0d9c11ebd39
KevinEpn <kevin.asimbaya@epn.edu.ec>
2024-03-02 20:44:04 -0500

    Insertar Empleado

--- BDProyecto/FormEmpleado.cs
+++ BDProyecto/FormEmpleado.cs
@@ -38,5 +38,22 @@ namespace BDProyecto
         {
             dataGridView1.DataSource = EmpleadoData.mostrar_empleados(conexion);
         }
+
+        private void btnGuardar_Click(object sender, EventArgs e)
+        {
+            string nombre_Empleado = txtNombreEmpleado.Text;
+            string apellido_empleado = txtApellidoEmpleado.Text;
+            string cedula_empleado = txtCedulaEmpleado.Text;
+            DateTime fecha_inicio = DateTime.Parse(txtFechaInicio.Text);
+            decimal salario = decimal.Parse(txtSalarioEmpleado.Text);
+            int cod_empleado = int.Parse(txtCodEmpleado.Text);
+            int cod_taller = int.Parse(txtCodTaller.Text);
+
+            Empleado empleado = new Empleado(cod_empleado, cod_taller, cedula_empleado, nombre_Empleado, apellido_empleado, salario, fecha_inicio);
+
+            EmpleadoData.insertar_empleado(empleado, this.conexion);
+
+            refressPantalla();
+        }
     }
 }
